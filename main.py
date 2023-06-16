97f9ccf120a715f37deea37e27b1b1390d929dd8
Dio <a0981551262@gmail.com>
2023-06-17 02:02:19 +0800

    finish programming To do list: 1. debug

--- main.py
+++ main.py
@@ -36,9 +36,10 @@ actions = env.action_space.shape[0]
 
 def build_model(states, actions):
     model = Sequential()
-    model.add(Dense(24, activation='relu', input_shape=states))
+    model.add(Dense(24, activation='relu', input_shape=(1, states[0])))
     model.add(Dense(24, activation='relu'))
     model.add(Dense(actions, activation='linear'))
+    model.add(Flatten())
     return model
 
 
