8df40deac2b15419ce456ee0a46d9ddae862e2d3
Raccoon103 <zthermau5@gmail.com>
2023-06-17 20:45:15 +0800

    Finished training; inference ready

--- gambling.py
+++ gambling.py
@@ -3,17 +3,20 @@ import random
 
 class Gambling:
 
-    def playing_baccarat(self, index, value):
+    def playing_baccarat(self, action, value):
 
-        percentage = (index % 10)+1 / 1000
-        hand = int(index/10)
-        odds = [0.95, 1, 8, 11]
-        playing_result = random.random()
-        print({'percentage': percentage, 'hand': hand})
         if value < 0:
             return 0
+
+        hand = int(action/10)
+        percentage = (action % 10 + 1)/10
+
+        percentage = percentage/100
+        odds = [0.95, 1, 8, 11]
+        playing_result = random.random()
+
         # banker win
-        elif playing_result < 0.458597 and hand == 0:
+        if playing_result < 0.458597 and hand == 0:
             return percentage * value * odds[hand]
 
         # player win
@@ -27,9 +30,5 @@ class Gambling:
         # pair
         elif hand == 3 and playing_result < 0.074683:
             return percentage * value * odds[hand]
-
-        # no action
-        elif index == 40:
-            return 0
 
         return -percentage * value
